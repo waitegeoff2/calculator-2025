dd93eb62d7a35f329a9647f68c3a0c194c5b05df
Geoff Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-01-14 12:54:58 -0500

    add basic functions for add, sub, div, mul

---
+++ java-calc.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1/num2;
+}
+
+console.log(add(4,2));
+
+console.log(subtract(4,8));
+
+console.log(multiply(2,16));
+
+console.log(divide(15,2));
